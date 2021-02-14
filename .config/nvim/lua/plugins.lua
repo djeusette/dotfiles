2b1b64a0a0fd72295af4d07bc12ab5274a6793af
David Jeusette <jeusette.david@gmail.com>
2021-02-14 19:35:29 +0100

    Use deoplete instead of completion-nvim

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -16,7 +16,7 @@ return require('packer').startup(function()
   -- Packer can manage itself as an optional plugin
   use {'wbthomason/packer.nvim', opt = true}
 
-  use {'scrooloose/nerdcommenter'}
+  use {'scrooloose/nerdcommenter', config = require('plugins.nerdcommenter') }
 
   -- Lazy loading:
   -- Load on specific commands
@@ -33,21 +33,25 @@ return require('packer').startup(function()
   }
   use {'lambdalisue/fern-git-status.vim'}
 
-  use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate', config = function() require('plugins.nvim-treesitter') end}
-  use {'neovim/nvim-lspconfig', config = require('plugins.lsp')}
+  use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate', config = require('plugins.nvim-treesitter') }
+  use {'neovim/nvim-lspconfig', config = require('plugins.lsp') }
 
   use {'ctrlpvim/ctrlp.vim', config = require('plugins.ctrlp')}
 
   use {'junegunn/fzf', run = fn['fzf#install']}
   use {'junegunn/fzf.vim', config = require('plugins.fzf') }
 
-  use {'NLKNguyen/papercolor-theme', config = require('plugins.papercolor')}
+  use {'NLKNguyen/papercolor-theme', config = require('plugins.papercolor') }
 
-  use {'tjdevries/express_line.nvim', requires = {{'nvim-lua/plenary.nvim'}}, config = require('plugins.express_line')}
+  use {'tjdevries/express_line.nvim', requires = {{'nvim-lua/plenary.nvim'}}, config = require('plugins.express_line') }
 
-  use {'nvim-lua/completion-nvim', config = require('plugins.completion-nvim')}
-  use {'steelsojka/completion-buffers'}
-  use {'aca/completion-tabnine', run = './install.sh', config = require('plugins.completion-tabnine')}
+  -- use {'nvim-lua/completion-nvim', config = require('plugins.completion-nvim')}
+  -- use {'steelsojka/completion-buffers'}
+  -- use {'aca/completion-tabnine', run = './install.sh', config = require('plugins.completion-tabnine')}
+
+  use {'shougo/deoplete-lsp'}
+  use {'shougo/deoplete.nvim', run = fn['remote#host#UpdateRemotePlugins'], config = require('plugins.deoplete') }
+  use {'tbodt/deoplete-tabnine', run = './install.sh'}
 
   use {'tpope/vim-projectionist'}
   use {'elixir-editors/vim-elixir'}
