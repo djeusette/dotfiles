dd529854960cde2a67b3f69f38085dbadd1c485e
David Jeusette <jeusette.david@gmail.com>
2022-10-19 16:49:48 +0200

    Fix buffer cycle mapping

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -18,11 +18,11 @@ lvim.colorscheme = "tokyonight"
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
 -- add your own keymapping
-lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+lvim.keys.normal_mode["<C-s>"] = ":w<CR>"
 
 -- Buffer movements
-lvim.keys.normal_mode["<C-H>"] = ":bp<cr>"
-lvim.keys.normal_mode["<C-L>"] = ":bn<cr>"
+lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
+lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 
 -- ; can be used instead of : in normal mode
 lvim.keys.normal_mode[";"] = ":"
