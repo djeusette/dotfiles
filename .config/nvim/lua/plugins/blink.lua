cb48d3d400e8bd026b628ab41642c004c7f4ab2f
David Jeusette <jeusette.david@gmail.com>
2025-01-03 22:05:51 +0100

    Update nvim config

---
+++ .config/nvim/lua/plugins/blink.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "saghen/blink.cmp",
+    ---@class PluginLspOpts
+    opts = {
+      signature = { enabled = true },
+      keymap = {
+        preset = "super-tab",
+      },
+    },
+  },
+}
