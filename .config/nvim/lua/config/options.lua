6e95483860c52408fa3cf69f15005996fa6eef9d
David Jeusette <jeusette.david@gmail.com>
2025-07-09 10:15:58 +0200

    Disable relative line numbers

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,3 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.autoformat = false
+
+local opt = vim.opt
+
+opt.relativenumber = false -- Disable relative line numbers
